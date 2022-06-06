d8dc7ff7197a2b846858815f4fdca34a4876990c
Kirill Katkov <k.katkov@dodopizza.com>
2022-06-06 10:06:09 +0300

    Add ApiTests

--- src/test/java/tests/ReqresinTests.java
+++ src/test/java/tests/ReqresinTests.java
@@ -7,40 +7,89 @@ import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
 public class ReqresinTests {
+
+    String baseUrl = "https://reqres.in",
+            loginUrl = "/api/login",
+            usersUrl = "/api/users",
+            users2Url = "/api/users/2",
+            createUserBody = "{\"name\": \"morpheus\",     \"job\": \"leader\" }",
+            updateUserBody = "{\"name\": \"morpheus\",\"job\": \"zion resident\"}",
+            loginBody = "{ \"email\": \"eve.holt@reqres.in\", " +
+                    "\"password\": \"cityslicka\" }",
+            missingPasswordBody = "{ \"email\": \"eve.holt@reqres.in\"}",
+            token = "QpwL5tke4Pnpja7X4",
+            error = "Missing password";
+
+    @Test
+    void getUsers() {
+        given()
+                .when()
+                .get(baseUrl + usersUrl)
+                .then()
+                .log().all()
+                .body("page", is(1))
+                .body("per_page", is(6))
+                .body("total", is(12))
+                .body("total_pages", is(2));
+    }
+
+    @Test
+    void createUser() {
+        given()
+                .body(createUserBody)
+                .contentType(JSON)
+                .when()
+                .post(baseUrl + usersUrl)
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void updateUser() {
+        given()
+                .body(updateUserBody)
+                .contentType(JSON)
+                .when()
+                .put(baseUrl + users2Url)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
     @Test
     void loginTest() {
-        String body = "{ \"email\": \"eve.holt@reqres.in\", " +
-                "\"password\": \"cityslicka\" }";
-
         given()
                 .log().uri()
                 .log().body()
-                .body(body)
+                .body(loginBody)
                 .contentType(JSON)
                 .when()
-                .post("https://reqres.in/api/login")
+                .post(baseUrl + loginUrl)
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(200)
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+                .body("token", is(token));
     }
 
     @Test
     void missingPasswordLoginTest() {
-        String body = "{ \"email\": \"eve.holt@reqres.in\"}";
-
         given()
                 .log().uri()
                 .log().body()
-                .body(body)
+                .body(missingPasswordBody)
                 .contentType(JSON)
                 .when()
-                .post("https://reqres.in/api/login")
+                .post(baseUrl + loginUrl)
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(400)
-                .body("error", is("Missing password"));
+                .body("error", is(error));
     }
 }
