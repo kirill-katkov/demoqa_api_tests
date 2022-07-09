2a13086f1fb20776b7c7e8921770335ce8f65d86
Kirill Katkov <k.katkov@dodopizza.com>
2022-07-09 11:21:42 +0300

    Add ApiTests

--- src/test/java/tests/BookStore.java
+++ src/test/java/tests/BookStore.java
@@ -30,20 +30,21 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 
 import static org.hamcrest.Matchers.*;
+
 @Tag("bookStore")
-public class BookStore extends TestBase {
+public class BookStore {
     static Credentials credentials = new Credentials();
 
 //godinew9
     //565656qqA!
     //{"userId":"293ed243-0866-49a8-a620-96af443e66f7","username":"godinew9",
 //    @BeforeAll
-//    static void beforeAll() {
-//        RestAssured.baseURI = "https://demoqa.com";
-//        System.setProperty("com.sun.security.enableAIAcaIssuers", "true");
-//        String certificatesTrustStorePath = "<JAVA HOME>/jre/lib/security/cacerts";
-//        System.setProperty("javax.net.ssl.trustStore", certificatesTrustStorePath);
-//    }
+    static void beforeAll() {
+        RestAssured.baseURI = "https://demoqa.com";
+        System.setProperty("com.sun.security.enableAIAcaIssuers", "true");
+        String certificatesTrustStorePath = "<JAVA HOME>/jre/lib/security/cacerts";
+        System.setProperty("javax.net.ssl.trustStore", certificatesTrustStorePath);
+    }
 
 
     @Test
